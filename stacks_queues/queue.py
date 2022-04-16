cf9aaf5f9f4f4e44dc8e2fe57dc305c83540f573
Stephanie Chen <stephanie.chenn1@gmail.com>
2022-04-16 19:37:34 -0400

    passed all tests for queue.py

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -29,35 +29,50 @@ class Queue:
         if self.size == 0:
             self.front = 0
             self.rear = 0
+
         self.store[self.rear] = element
-        self.rear = (self.rear+ 1) % self.buffer_size
+        self.rear = (self.rear + 1) % self.buffer_size
+        self.size += 1
 
     def dequeue(self):
         """ Removes and returns an element from the Queue
             Raises a QueueEmptyException if 
             The Queue is empty.
         """
-        pass
+        if self.size == 0:
+            raise QueueEmptyException("Queue is empty!")
+    
+        front_ele = self.store[self.front]
+        self.front = (self.front + 1) % self.buffer_size
+        self.size -= 1
+        return front_ele
+
 
     def front(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
             is empty.  Does not remove anything.
         """
-        pass
+        if not self.store:
+            return None
+        return self.store[0]
         
 
     def size(self):
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        return self.store.size
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        if self.size == 0:
+            return True
+        else:
+            return False
+
 
     def __str__(self):
         """ Returns the Queue in String form like:
@@ -65,4 +80,10 @@ class Queue:
             Starting with the front of the Queue and
             ending with the rear of the Queue.
         """
-        pass
+        str_list_form = []
+        start = self.front
+
+        for i in range(self.size):
+            str_list_form.append(self.store[start])
+            start = (start + 1) % self.buffer_size
+        return str(str_list_form)
