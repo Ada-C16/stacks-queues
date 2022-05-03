610997f32d915c52bc221db7a5649df69a17953c
lizett <lizett503@gmail.com>
2022-05-02 17:24:53 -0700

    All tests pass

--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -12,7 +12,7 @@ class Stack:
         """ Adds an element to the top of the Stack.
             Returns None
         """
-        pass
+        self.store.add_first(element)
 
     def pop(self):
         """ Removes an element from the top
@@ -21,13 +21,15 @@ class Stack:
             The Stack is empty.
             returns None
         """
-        pass
+        if self.empty():
+            raise StackEmptyException("stack is empty")
+        return self.store.remove_first()
 
     def empty(self):
         """ Returns True if the Stack is empty
             And False otherwise
         """
-        pass
+        return not self.store.get_first()
 
     def __str__(self):
         """ Returns the Stack in String form like:
@@ -35,4 +37,4 @@ class Stack:
             Starting with the top of the Stack and
             ending with the bottom of the Stack.
         """
-        pass
+        return "[" + str(self.store) + "]"
