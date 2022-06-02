e57abb9cc8ccfacff8235a6cc1d14755cd3a41bf
Veronica Osei <veronicaosei273@gmail.com>
2022-06-02 13:51:21 -0500

    tests passing

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -1,4 +1,8 @@
 
+from re import T
+from xml.dom.minidom import Element
+
+
 INITIAL_QUEUE_SIZE = 20
 
 class QueueFullException(Exception):
@@ -23,34 +27,62 @@ class Queue:
             In the store are occupied
             returns None
         """
-        pass
+        if self.buffer_size == self.size:
+            raise QueueFullException
+        if (self.rear + 1) == self.buffer_size:
+            self.rear = 0
+            self.size += 1
+            self.store[self.rear] = element
+        else: 
+            self.rear += 1
+            self.size += 1
+            self.store[self.rear] = element
+           
+
+
 
     def dequeue(self):
         """ Removes and returns an element from the Queue
             Raises a QueueEmptyException if 
             The Queue is empty.
         """
-        pass
+        if self.size == 0:
+            raise QueueEmptyException
+        if (self.front + 1) == self.buffer_size:
+            self.front = 0
+            self.size -= 1
+            return self.store[self.front]
+        else:
+            self.front += 1
+            self.size -= 1
+            return self.store[self.front]
+
 
     def front(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
             is empty.  Does not remove anything.
         """
-        pass
+        if self.size == 0:
+            return None
+        else:
+            return self.store[self.front]
         
 
     def size(self):
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        return self.size
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        if self.size == 0:
+            return True
+        else:
+            return False
 
     def __str__(self):
         """ Returns the Queue in String form like:
@@ -58,4 +90,10 @@ class Queue:
             Starting with the front of the Queue and
             ending with the rear of the Queue.
         """
-        pass
+        index = self.front
+        lst = []
+        for _ in range(self.size):
+            lst.append(self.store[(index +1) % self.buffer_size])
+            index += 1
+        return str(lst)
+
