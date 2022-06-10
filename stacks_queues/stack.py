af849b0157f2d318563e718aa8e3765f4abe998b
Monica Cruz <mcatcruz@gmail.com>
2022-06-09 18:49:11 -0700

    Passing tests for push, pop, and empty

--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -12,7 +12,7 @@ class Stack:
         """ Adds an element to the top of the Stack.
             Returns None
         """
-        pass
+        self.store.add_first(element)
 
     def pop(self):
         """ Removes an element from the top
@@ -21,13 +21,21 @@ class Stack:
             The Stack is empty.
             returns None
         """
-        pass
+        if self.store.empty():
+            raise StackEmptyException
+        else:
+            return self.store.remove_first()
+
+
+
+
+
 
     def empty(self):
         """ Returns True if the Stack is empty
             And False otherwise
         """
-        pass
+        return self.store.empty()
 
     def __str__(self):
         """ Returns the Stack in String form like:
