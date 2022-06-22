173fa69984466c14db8026c157e9d108c5bd62c3
Monica Cruz <mcatcruz@gmail.com>
2022-06-21 22:29:28 -0700

    Incremented self.size in enqueue and decremented self.size in dequeue

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -22,7 +22,13 @@ class Queue:
             In the store are occupied
             returns None
         """
-        if (self.front == 0 and self.rear == self.size -1) or (self.rear == (self.front-1)%(self.size-1)):
+        
+        if self.front == (self.rear+1 % self.buffer_size):
+            # Re: (self.front == 0 and self.rear == self.size -1) 
+            # Should be checking self.rear against BUFFER_SIZE, not self.size. Otherwise, this expression will always return True even though the Queue still has spaces left. 
+            # Note that self.front is not always at index 0.
+            # Re: modulo operator
+            # self.rear + 1 is the same as self.rear +1 % buffer_size in any case except for where rear is the last index of the list (where self.rear = 19), need to go back to 0 (wrap around to the front)
             raise QueueFullException
         elif (self.front == -1):
             self.front = 0
@@ -34,26 +40,35 @@ class Queue:
         else:
             self.rear += 1
             self.store[self.rear] = element
+        
+        self.size += 1
+
 
     def dequeue(self):
         """ Removes and returns an element from the Queue
             Raises a QueueEmptyException if 
             The Queue is empty.
         """
+        element_to_be_dequeued = self.store[self.front]
+        self.store[self.front] = None
+
         if self.front == -1:
             raise QueueEmptyException
 
-        element = self.store[self.front]
-        self.store[self.front] = None
-
-        if (self.front == self.rear):
+        elif self.front == self.rear:
             self.front = -1
             self.rear = -1
-        elif (self.front == self.size-1):
+
+        elif self.front == self.buffer_size-1:
             self.front = 0
+
         else:
-            self.front += 1
-        return element
+            self.front = (self.front + 1) % self.buffer_size
+
+        self.size -= 1
+
+        return element_to_be_dequeued
+
     def front(self):
         """ Returns an element from the front
             of the Queue and None if the Queue
@@ -61,7 +76,6 @@ class Queue:
         """
         return self.store[self.front]
         
-
     def size(self):
         """ Returns the number of elements in
             The Queue
@@ -72,7 +86,7 @@ class Queue:
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        return self.size > 1
+        return self.size == 0
 
     def __str__(self):
         """ Returns the Queue in String form like:
