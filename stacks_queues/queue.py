6117dc6e9f5ca12c1ec02afed47c0c592cc582f7
sjolivas <sjolivas@gmail.com>
2022-07-18 17:41:27 -0600

    all stacks required tests passing

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -1,14 +1,15 @@
-
 INITIAL_QUEUE_SIZE = 20
+
 
 class QueueFullException(Exception):
     pass
 
+
 class QueueEmptyException(Exception):
     pass
 
-class Queue:
 
+class Queue:
     def __init__(self):
         self.store = [None] * INITIAL_QUEUE_SIZE
         self.buffer_size = INITIAL_QUEUE_SIZE
@@ -17,44 +18,43 @@ class Queue:
         self.size = 0
 
     def enqueue(self, element):
-        """ Adds an element to the Queue
-            Raises a QueueFullException if all elements
-            In the store are occupied
-            returns None
+        """Adds an element to the Queue
+        Raises a QueueFullException if all elements
+        In the store are occupied
+        returns None
         """
         pass
 
     def dequeue(self):
-        """ Removes and returns an element from the Queue
-            Raises a QueueEmptyException if 
-            The Queue is empty.
+        """Removes and returns an element from the Queue
+        Raises a QueueEmptyException if
+        The Queue is empty.
         """
         pass
 
     def front(self):
-        """ Returns an element from the front
-            of the Queue and None if the Queue
-            is empty.  Does not remove anything.
+        """Returns an element from the front
+        of the Queue and None if the Queue
+        is empty.  Does not remove anything.
         """
         pass
-        
 
     def size(self):
-        """ Returns the number of elements in
-            The Queue
+        """Returns the number of elements in
+        The Queue
         """
         pass
 
     def empty(self):
-        """ Returns True if the Queue is empty
-            And False otherwise.
+        """Returns True if the Queue is empty
+        And False otherwise.
         """
         pass
 
     def __str__(self):
-        """ Returns the Queue in String form like:
-            [3, 4, 7]
-            Starting with the front of the Queue and
-            ending with the rear of the Queue.
+        """Returns the Queue in String form like:
+        [3, 4, 7]
+        Starting with the front of the Queue and
+        ending with the rear of the Queue.
         """
         pass
