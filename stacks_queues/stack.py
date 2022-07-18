faae81da702f3c522cb9abc9c50e851ede9022cd
Jiajia Wang <grace.wangjiajia@gmail.com>
2022-07-18 15:05:00 -0700

    stack

--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -9,30 +9,13 @@ class Stack:
         self.store = LinkedList()
 
     def push(self, element):
-        """ Adds an element to the top of the Stack.
-            Returns None
-        """
-        pass
+        self.store.add_first(element)
 
     def pop(self):
-        """ Removes an element from the top
-            Of the Stack
-            Raises a StackEmptyException if
-            The Stack is empty.
-            returns None
-        """
-        pass
+        return self.store.remove_first()
 
     def empty(self):
-        """ Returns True if the Stack is empty
-            And False otherwise
-        """
-        pass
+        return self.store.empty()
 
     def __str__(self):
-        """ Returns the Stack in String form like:
-            [3, 4, 7]
-            Starting with the top of the Stack and
-            ending with the bottom of the Stack.
-        """
-        pass
+        return str(self.store)
