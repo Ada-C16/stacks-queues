ff3f34b810cd74c373f76b4188955941d2f6450b
mac-madison <macanmadison@gmail.com>
2022-07-21 12:57:46 -0700

    passing

--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -1,38 +1,39 @@
 from stacks_queues.linked_list import LinkedList
 
+
 class StackEmptyException(Exception):
     pass
 
-class Stack:
 
+class Stack:
     def __init__(self):
         self.store = LinkedList()
 
     def push(self, element):
-        """ Adds an element to the top of the Stack.
-            Returns None
+        """Adds an element to the top of the Stack.
+        Returns None
         """
-        pass
+        self.store.add_first(element)
 
     def pop(self):
-        """ Removes an element from the top
-            Of the Stack
-            Raises a StackEmptyException if
-            The Stack is empty.
-            returns None
+        """Removes an element from the top
+        Of the Stack
+        Raises a StackEmptyException if
+        The Stack is empty.
+        returns None
         """
-        pass
+        return self.store.remove_first() if not self.empty() else StackEmptyException()
 
     def empty(self):
-        """ Returns True if the Stack is empty
-            And False otherwise
+        """Returns True if the Stack is empty
+        And False otherwise
         """
-        pass
+        return self.store.empty()
 
     def __str__(self):
-        """ Returns the Stack in String form like:
-            [3, 4, 7]
-            Starting with the top of the Stack and
-            ending with the bottom of the Stack.
+        """Returns the Stack in String form like:
+        [3, 4, 7]
+        Starting with the top of the Stack and
+        ending with the bottom of the Stack.
         """
-        pass
+        return self.store.visit()
