236a28fe2b0a30e211085afa9493f2c2ebd24911
letypl12 <86847701+letypl12@users.noreply.github.com>
2022-07-21 17:34:07 -0700

    Completed queue and stack project

--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -4,6 +4,9 @@ class StackEmptyException(Exception):
     pass
 
 class Stack:
+    #This stack implementation overloads the LinkedList underneath it
+    #to make life easy.  In true software dev fashion, no need to 
+    #reinvent the wheel.
 
     def __init__(self):
         self.store = LinkedList()
@@ -12,7 +15,8 @@ class Stack:
         """ Adds an element to the top of the Stack.
             Returns None
         """
-        pass
+        return self.store.add_first(element)
+        
 
     def pop(self):
         """ Removes an element from the top
@@ -21,13 +25,19 @@ class Stack:
             The Stack is empty.
             returns None
         """
-        pass
+        if self.store.length == 0:
+            raise StackEmptyException ("stack is empty")
+
+        return self.store.remove_first()
 
     def empty(self):
         """ Returns True if the Stack is empty
             And False otherwise
         """
-        pass
+        self.store.length = 0
+        if self.store.length == 0:
+            return True
+        return False
 
     def __str__(self):
         """ Returns the Stack in String form like:
@@ -35,4 +45,5 @@ class Stack:
             Starting with the top of the Stack and
             ending with the bottom of the Stack.
         """
-        pass
+        return self.store.__str__
+
